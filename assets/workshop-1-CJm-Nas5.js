1521855244c0cdd9cd36bff3fbb985443c43b860
KalyoteX <46811118+Trooy96@users.noreply.github.com>
2026-04-25 09:09:35 +0200

    Add files via upload

---
+++ assets/workshop-1-CJm-Nas5.js
@@ -0,0 +1,1 @@
+const s="/assets/mining-bell-5ye7uJhd.jpg",a="/assets/energy-solar-Dqop2CaI.jpg",e="/assets/agriculture-tractor-BqsvG5x-.jpg",o="/assets/workshop-1-bPyCT-BE.jpg";export{e as a,a as e,s as m,o as w};
